0959ec384dd10d174cc806989cdda5adc99f8c6f
EC2 Default User <ec2-user@ip-172-31-15-68.us-east-2.compute.internal>
2018-04-17 15:52:33 +0000

    Ready for class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Author.create name: 'William Shakespeare'
+Author.create name: 'John Keats'
+Author.create name: 'F. Scott Fitzgerald'
+
+Book.create title: 'Hamlet', price: 1500, hardcover: false
+Book.create title: 'The Nightengale', price: 2995, hardcover: true
+Book.create title: 'The Great Gatsby', price: 1495, hardcover: false
+
+print "There are now #{Author.count} authors in the database.\n"
+print "There are now #{Book.count} books in the database.\n"
